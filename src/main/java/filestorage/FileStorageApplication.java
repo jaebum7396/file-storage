3d0aae8c9f2449d33a274464c193d25e85f6afd3
jaebeom <jaebum7396@naver.com>
2023-05-16 00:51:20 +0900

    refactor. 프로필 및 라우팅 설정 추가

--- src/main/java/filestorage/FileStorageApplication.java
+++ src/main/java/filestorage/FileStorageApplication.java
@@ -3,9 +3,12 @@ package filestorage;
 import filestorage.configuration.FileStorageConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 @EnableConfigurationProperties(FileStorageConfig.class)
 public class FileStorageApplication {
 	public static void main(String[] args) {
---
+++ src/main/java/filestorage/configuration/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package filestorage.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+	@Value("${cloud.gateway.uri}")
+	String GATEWAY_URI;
+	@Value("${spring.profiles.active}")
+	String ACTIVE_PROFILE;
+	@Override
+	public void addInterceptors(InterceptorRegistry reg) {
+		reg.addInterceptor(new MyInterceptor(GATEWAY_URI, ACTIVE_PROFILE))
+				.addPathPatterns("/*")
+				.excludePathPatterns("/css/**", "/images/**", "/js/**");
+	}
+}
