1293d4dae6a3d0863c82ccfb0f80c6d51876e2f4
jaebeom <jaebum7396@naver.com>
2023-05-16 01:01:09 +0900

    refactor. 프로필 및 라우팅 설정 추가

--- src/main/java/filestorage/controller/FileStorageController.java
+++ src/main/java/filestorage/controller/FileStorageController.java
@@ -20,7 +20,6 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/file-storage")
 public class FileStorageController {
 
     private static final Logger logger = LoggerFactory.getLogger(FileStorageController.class);
